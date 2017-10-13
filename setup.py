bf9bd0f289a99839e7af9e129152ea099317d481
seantyh <seantyh@crl.psy.ntu.edu.tw>
2017-10-13 10:42:08 +0800

    init commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='LingPlus',
+      version='0.1',
+      description='Linguistics Plus X',
+      url='http://github.com/seantyh/lingplus',
+      author='Sean Tseng',
+      author_email='seantyh@gmail.com',
+      license='GNUv3',
+      packages=['LingPlus'],
+      install_requires=[],
+      test_suite='test',
+      zip_safe=False)
