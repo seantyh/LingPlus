41776f0c28c6ca31ca627a1ead5c8c6a76f637ab
seantyh <seantyh@crl.psy.ntu.edu.tw>
2017-10-13 10:43:04 +0800

    update url in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='LingPlus',
       version='0.1',
       description='Linguistics Plus X',
-      url='http://github.com/seantyh/lingplus',
+      url='https://github.com/seantyh/LingPlus',
       author='Sean Tseng',
       author_email='seantyh@gmail.com',
       license='GNUv3',
